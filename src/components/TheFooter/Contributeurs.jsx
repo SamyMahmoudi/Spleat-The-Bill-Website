be2c06e04182fe9482f7f03ae2bff5fd395b3119
SamyMahmoudi <samy.mahmoudi@edu.devinci.fr>
2022-04-22 12:09:02 +0200

    Fix links

--- src/components/TheFooter/Contributeurs.jsx
+++ src/components/TheFooter/Contributeurs.jsx
@@ -3,10 +3,10 @@ const Contributeurs = () => {
         <div className="TheFooter-Container">
             <h3>Contributeurs</h3>
             <ul>
-                <li><a href="https://github.com/SamyMahmoudi" target="_blank">Samy Mahmoudi</a></li>
-                <li><a href="https://github.com/aleperdriel" target="_blank">Auriane Le Perdriel</a></li>
-                <li><a href="https://github.com/Alexis-Bally" target="_blank">Alexis Bally</a></li>
-                <li><a href="https://github.com/ClemOurs" target="_blank">Clément Duvivier</a></li>
+                <li><a href="https://github.com/SamyMahmoudi" target="_blank" title="Samy Mahmoudi" rel="noreferrer">Samy Mahmoudi</a></li>
+                <li><a href="https://github.com/aleperdriel" target="_blank" title="Auriane Le Perdriel" rel="noreferrer">Auriane Le Perdriel</a></li>
+                <li><a href="https://github.com/Alexis-Bally" target="_blank" title="Alexis Bally" rel="noreferrer">Alexis Bally</a></li>
+                <li><a href="https://github.com/ClemOurs" target="_blank" title="Clément Duvivier" rel="noreferrer">Clément Duvivier</a></li>
             </ul>
         </div>
     )
