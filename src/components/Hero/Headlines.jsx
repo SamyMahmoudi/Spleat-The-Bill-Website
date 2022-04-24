d92b55b0213f2fb51d2315ceed84ee773583d944
SamyMahmoudi <samy.mahmoudi@edu.devinci.fr>
2022-04-24 15:31:27 +0200

    Fix hero section button anchor link

--- src/components/Hero/Headlines.jsx
+++ src/components/Hero/Headlines.jsx
@@ -1,11 +1,9 @@
-import { NavLink } from "react-router-dom";
-
 const Headlines = () => {
     return (
         <div className="HeroSection-Headlines">
             <h1>Spleat The Bill</h1>
             <p>Payez et partagez votre addition rapidement et sans prise de tête</p>
-            <NavLink to="#Histoire" title="En savoir plus">En savoir plus</NavLink>
+            <a href="#Histoire" title="En savoir plus">En savoir plus</a>
         </div>
     );
 };
